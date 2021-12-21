23a781c3f23019720f782922a869070069edfc0f
alejandrosansilverio <66685630+alejandrosansilverio@users.noreply.github.com>
2021-12-21 18:15:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -12,4 +13,4 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(PORT);
